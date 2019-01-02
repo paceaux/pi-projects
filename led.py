faabe5f9d641da87ea56e77ab1e803041b19f4f7
paceaux <frank.m.taylor08@gmail.com>
2019-01-02 14:10:43 -0600

    adds blinking and toggling. also state

--- led.py
+++ led.py
@@ -5,11 +5,19 @@ import math
 class Led:
     def __init__(self, pin):
         self.pin = pin
+        self.isOn = False
         GPIO.setup(self.pin, GPIO.OUT)
     def on(self):
+        self.isOn = True
         GPIO.output(self.pin, True)
     def off(self):
+        self.isOn = False
         GPIO.output(self.pin, False)
+    def toggle(self):
+        if (self.isOn == True) :
+            self.off()
+        else:
+            self.on()
     def blink(self, cycles = 1, interval = .25):
         counter = 0
         
