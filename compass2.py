3c9e3d4033e35515fbb683c61e162ca1bbfcaa0c
paceaux <frank.m.taylor08@gmail.com>
2019-02-02 21:55:22 -0600

    makes sure I know it's only working in python3

--- compass2.py
+++ compass2.py
@@ -1,13 +1,15 @@
+# ONLY WORKS IN PYTHON3
+
 import py_qmc5883l
 
 sensor = py_qmc5883l.QMC5883L()
 sensor.mode_continuous()
-sensor.declination = -4
+sensor.declination = 0
 m = sensor.get_magnet()
-
+bearing = sensor.get_bearing()
 
 
 
 print(m)
-
+print(bearing)
 
