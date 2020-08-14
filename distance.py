c99b7d5219c51050abaed6693d7061223bb4f047
paceaux <frank.m.taylor08@gmail.com>
2020-08-14 16:00:17 -0500

    gets distance working!

--- distance.py
+++ distance.py
@@ -1,61 +1,54 @@
-# pimylifeup.com/raspberry-pi-distance-sensor/
-# this maybe ran successfully once?
-# supposed to work for the HC-SR04 sensor
-
+#  https://tutorials-raspberrypi.com/raspberry-pi-ultrasonic-sensor-hc-sr04/
+# IT WORKS!!!!!!!
 import RPi.GPIO as GPIO
 import time
 
+GPIO.setmode(GPIO.BCM)
 
+GPIO_TRIGGER = 18
+GPIO_ECHO = 24
 
-GPIO.setmode(GPIO.BOARD)
-
-PIN_TRIGGER = 7
-PIN_ECHO = 11
-
-GPIO.setup(PIN_TRIGGER, GPIO.OUT)
-GPIO.setup(PIN_ECHO, GPIO.IN)
-
-
-
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
 
 def distance():
-    GPIO.output(PIN_TRIGGER, GPIO.LOW)
-
-    time.sleep(0.00001)
-    GPIO.output(PIN_TRIGGER, False)
-
-    pulse_start =time.time()
-    pulse_end = time.time()
-   
-    print("first pulse start" + str(pulse_start))
-    while (GPIO.input(PIN_ECHO)==0):
-        pulse_start = time.time()
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
  
-    """ note: Never get to the second pulse. 
-        Looks like the input never gets to 1
-        When I print GPIO.input(PIN_ECHO), it's always zero. never gets beyond that
-        Do I have a wiring problem?
-    """
-    while GPIO.input(PIN_ECHO) == 1:
-        pulse_end= time.time()
-
-       
-    pulse_duration = pulse_end - pulse_start
-    distance = round(pulse_duration * 17150, 2)
-
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+ 
+    StartTime = time.time()
+    StopTime = time.time()
+ 
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+ 
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+ 
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+ 
     return distance
-
-if __name__ =='__main__':
+ 
+if __name__ == '__main__':
     try:
         while True:
-            print("trying to get distance")
-            GPIO.output(PIN_TRIGGER, GPIO.LOW)
-            print("Let sensor settle")
-            time.sleep(2)
             dist = distance()
-            print("Distance is " + str(dist) + " cm")
-            time.sleep(1)
-    except KeyboardInterrupt:
-        print("stopped measuring")
+            distInches = dist / 2.54
 
+            print ("Measured Distance = %.1f cm" % dist)
+            print ("Measured Distance = %.1f inches" % distInches)
+            time.sleep(1)
+ 
+        # Reset by pressing CTRL + C
+    except KeyboardInterrupt:
+        print("Measurement stopped by User")
         GPIO.cleanup()
