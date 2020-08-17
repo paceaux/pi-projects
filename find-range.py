2e03f2a7ead04456967f8caa123555f8828257ce
paceaux <frank.m.taylor08@gmail.com>
2020-08-17 11:55:03 -0500

    adds ability to indicate range w/ LED

--- find-range.py
+++ find-range.py
@@ -9,13 +9,19 @@ GPIO_ECHO = 24
 if __name__ == '__main__':
     try:
         sensor = DistanceSensor(GPIO_TRIGGER, GPIO_ECHO)
+        yellow = Led(19)
+        green = Led(17)
         while True:
             dist = sensor.getDistance()
-            print ("Measured Distance = %.1f inches" % dist.inches)
             if (dist.feet > 1):
                 print("Measured Distance = %.1f feet" % dist.feet)
+                green.blink(round(dist.feet), (1 / dist.feet))
+            if (dist.feet < 1):
+                print("Measured Distance = %.1f inches" % dist.inches)
+            
+                yellow.blink(round(dist.inches), (1/ dist.inches) - .02)
 
-            time.sleep(.5)
+            time.sleep(2)
  
         # Reset by pressing CTRL + C
     except KeyboardInterrupt:
