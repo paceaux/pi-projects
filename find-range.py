12723091edb1c118cb1a0569abed8a9999090f4c
Frank Taylor <Frank.Taylor@exlrt.com>
2020-08-20 10:48:56 -0500

    adds comments, code for second sensor

--- find-range.py
+++ find-range.py
@@ -4,8 +4,8 @@ from led import Led
 from Sensors.DistanceSensor import DistanceSensor
 
 # Known working pins for the hcsr04 sensor
-GPIO_TRIGGER = 18
-GPIO_ECHO = 24
+GPIO_TRIGGER = 18 # should be PWM pins: 12, 13, 18, 19
+GPIO_ECHO = 24 # generic ones: 17, 27, 22, 23, 24, 25
 
 # how much time between scans
 INTERVAL_TIME = .5
@@ -13,7 +13,8 @@ INTERVAL_TIME = .5
 if __name__ == '__main__':
     try:
         # Create a sensor
-        sensor = DistanceSensor(GPIO_TRIGGER, GPIO_ECHO)
+        sensor1 = DistanceSensor(GPIO_TRIGGER, GPIO_ECHO)
+        sensor2 = DistanceSensor(12, 23);
         # create some Leds
         closeLight = Led(19)
         farLight = Led(17)
@@ -21,22 +22,24 @@ if __name__ == '__main__':
         # Create an infinite loop
         while True:
             # Get the distance (returns a Distance object so we can get cm, m, ft, in off of it)
-            dist = sensor.getDistance()
+            dist1 = sensor1.getDistance()
+            dist2 = sensor2.getDistance()
 
             # do some stuff
-            if (dist.feet > 1):
-                print("Measured Distance = %.1f feet" % dist.feet)
+            print(dist2.inches)
+            if (dist1.feet > 1):
+                print("Measured Distance = %.1f feet" % dist1.feet)
                 # Blink the number of times, rounding
                 # also set the Interval to blink to be less than a second. This way we can set our loop to at least 1 second and get all our blinks in
-                farLight.blink(round(dist.feet), (INTERVAL_TIME / dist.feet))
-            if (dist.feet < 1):
-                print("Measured Distance = %.1f inches" % dist.inches)
+                farLight.blink(round(dist1.feet), (INTERVAL_TIME / dist1.feet))
+            if (dist1.feet < 1):
+                print("Measured Distance = %.1f inches" % dist1.inches)
             
-                closeLight.blink(round(dist.inches), (INTERVAL_TIME/ dist.inches))
+                closeLight.blink(round(dist1.inches), (INTERVAL_TIME/ dist1.inches))
 
             time.sleep(INTERVAL_TIME)
  
         # Reset by pressing CTRL + C
     except KeyboardInterrupt:
         print("Measurement stopped by User")
-        sensor.tearDown()
+        sensor1.tearDown()
