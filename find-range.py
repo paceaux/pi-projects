026c92a9627cbe1a59c66f3fb0b4b1e4f8cfbc08
paceaux <frank.m.taylor08@gmail.com>
2020-08-20 11:36:37 -0500

    creates separate functions to blink and print distances

--- find-range.py
+++ find-range.py
@@ -10,14 +10,24 @@ GPIO_ECHO = 24 # generic ones: 17, 27, 22, 23, 24, 25
 # how much time between scans
 INTERVAL_TIME = .5
 
+def printDistance(distance, label):
+    if (distance.feet > 1):
+        print(label + " = %.1f feet" % distance.feet)
+
+    if (dist2.feet < 1):
+        print(label + " = %.1f inches" % distance.inches)
+
+def blinkDistance(amount, led):
+    led.blink(amount, INTERVAL_TIME / amount)
+
 if __name__ == '__main__':
     try:
         # Create a sensor
         sensor1 = DistanceSensor(GPIO_TRIGGER, GPIO_ECHO)
         sensor2 = DistanceSensor(12, 23);
         # create some Leds
-        closeLight = Led(19)
-        farLight = Led(17)
+        led1 = Led(19)
+        led2 = Led(17)
 
         # Create an infinite loop
         while True:
@@ -26,17 +36,10 @@ if __name__ == '__main__':
             dist2 = sensor2.getDistance()
 
             # do some stuff
-            print(dist2.inches)
-            if (dist1.feet > 1):
-                print("Measured Distance = %.1f feet" % dist1.feet)
-                # Blink the number of times, rounding
-                # also set the Interval to blink to be less than a second. This way we can set our loop to at least 1 second and get all our blinks in
-                farLight.blink(round(dist1.feet), (INTERVAL_TIME / dist1.feet))
-            if (dist1.feet < 1):
-                print("Measured Distance = %.1f inches" % dist1.inches)
-            
-                closeLight.blink(round(dist1.inches), (INTERVAL_TIME/ dist1.inches))
-
+            printDistance(dist1, "left Sensor")
+            printDistance(dist2, "right Sensor")
+            blinkDistance(dist1.inches, led1 )
+            blinkDistance(dist2.inches, led2 )
             time.sleep(INTERVAL_TIME)
  
         # Reset by pressing CTRL + C
