c9eb1f138868d41f8304c4251382cc4a6d161ba8
paceaux <frank.m.taylor08@gmail.com>
2020-09-15 15:50:55 -0500

    gets compass display to behave like proper compass. kinda

--- show-bearings.py
+++ show-bearings.py
@@ -3,19 +3,20 @@ import time
 import os
 import datetime
 from PIL import ImageFont
-from get_oled import get_device
+from Utils.get_oled import get_device
 from luma.core.render import canvas
 from Sensors.Compass import Compass
 
-def posn(angle, arm_length):
-    dx = int(math.cos(math.radians(angle)) * arm_length)
-    dy = int(math.sin(math.radians(angle)) * arm_length)
+def posn(angle, radius):
+    dx = int(math.cos(math.radians(angle)) * radius)
+    dy = int(math.sin(math.radians(angle)) * radius)
 
     return (dx, dy)
 
 def main():
     margin = 10
-    cx = 30
+    # center of device
+    cx = min(device.width, 128) / 2
     cy = min(device.height, 64) / 2
     sensor = Compass();
 
@@ -25,13 +26,26 @@ def main():
 
 
     with canvas(device) as draw:
-        bearing = sensor.get_bearing()
+        # reduce the bearing to a reasonable number
+        bearing = round(sensor.get_bearing(), 1)
 
-        draw.text((cx,0), "N", font=font, fill="white" )
-        draw.ellipse((left + margin, margin, right - margin, min(device.height, 64) - margin), outline="white" )
-        hrs = posn(bearing, cy - margin - 7)
+        #unsure of exactly why have to use 270 here. I'm guessing the circle doesn't start at the top, at 0 degrees?
+        north = posn(bearing + 270, cy - margin);
+        south = posn(bearing - 90, cy - margin)
 
-        draw.line((cx, cy, cx + hrs[0], cy + hrs[1]), fill="white")
+        #output bearing to upper right corner
+        draw.text((device.width - 30, 0), str(bearing), font=font, fill="red")
+        #put an "N" where north would be
+        draw.text((cx + north[0], cy + north[1]), "N", font=font, fill="white" )
+
+        #Draw the rings of the compass
+        draw.ellipse(
+            (left + margin, 
+            margin, 
+            right - margin, min(device.height, 64) - margin),
+             outline="white" )
+
+        draw.line((cx - south[0], cy - south[1], cx + north[0], cy + north[1]), fill="white")
 
     time.sleep(0.1)
 
